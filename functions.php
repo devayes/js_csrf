4f826958e6e75e36e73b92ef003e21e22322cf2f
Devin Hayes <devayes@gmail.com>
2012-12-07 01:39:50 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -53,9 +53,9 @@ function simple_encrypt($str='', $key='' )
         $keychar = substr($key, ($i % strlen($key)) -1, 1);
         $char = chr(ord($char) + ord($keychar));
         $result .= $char;
-	}
+    }
 
-	return base64_encode($result);
+    return base64_encode($result);
 }
      
 /**

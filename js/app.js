4ffbdcfe59fc772a5b1bc75e82175f99f7a34879
Devin Hayes <devayes@gmail.com>
2014-01-16 13:10:26 -0800

    Update app.js
    Notes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 var OBJ={
+    // Change the domain here
     cookie:{set:function(n,v,e){document.cookie=n+'='+escape(v)+((e)?';expires='+e.toUTCString():'')+';domain=localhost;path=/';},get:function(n){n=n+'=';var c=document.cookie,t=c.indexOf(n);if(t==-1){return '';}var v=c.indexOf(';',t+n.length);if(v==-1){v=c.length;}return unescape(c.substring(t+n.length,v));}},
+    // Change the url to your tokenizer
     getToken:function(){var token=$.ajax({type:"POST",data:'ajax=1',url:'ajax.php',async:false,cache:false}).responseText;OBJ.cookie.set('atok',token);return token;}
 }
 
 $(function(){
-    $('form').submit(function(){
-        var tok = OBJ.getToken();
+    // All forms will generate a token on submit
+    $('form').on('submit', function(){
+        OBJ.getToken();
+        return true;
+    });
+    // Works for links too:
+    // <a class="csrf" href="safe_page.php">link</a>
+    $('a.csfr').on('click', function(){
+        OBJ.getToken();
         return true;
     });
 });

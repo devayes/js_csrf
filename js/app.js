da2491d6205b734cdc4d9bb21ae9fd2c5a7780dc
Devin Hayes <devayes@gmail.com>
2014-06-17 11:27:06 -0700

    Update app.js
    added support for https, retrieve domain from window object.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+var domain = window.location.hostname;
 var OBJ={
-    // Change the domain here
-    cookie:{set:function(n,v,e){document.cookie=n+'='+escape(v)+((e)?';expires='+e.toUTCString():'')+';domain=localhost;path=/';},get:function(n){n=n+'=';var c=document.cookie,t=c.indexOf(n);if(t==-1){return '';}var v=c.indexOf(';',t+n.length);if(v==-1){v=c.length;}return unescape(c.substring(t+n.length,v));}},
+    // Get basePath
+    getOrigin:function(){ return window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port: ''); },
+    // Cookie set and get mehods.
+    cookie:{set:function(n,v,e){document.cookie=n+'='+escape(v)+((e)?';expires='+e.toUTCString():'')+';domain='+domain+';path=/';},get:function(n){n=n+'=';var c=document.cookie,t=c.indexOf(n);if(t==-1){return '';}var v=c.indexOf(';',t+n.length);if(v==-1){v=c.length;}return unescape(c.substring(t+n.length,v));}},
     // Change the url to your tokenizer
-    getToken:function(){var token=$.ajax({type:"POST",data:'ajax=1',url:'ajax.php',async:false,cache:false}).responseText;OBJ.cookie.set('atok',token);return token;}
+    getToken:function(){var token=$.ajax({type:"POST",data:'ajax=1',url:OBJ.getOrigin()+'/ajax.php',async:false,cache:false}).responseText;OBJ.cookie.set('atok',token);return token;}
 }
 
 $(function(){
